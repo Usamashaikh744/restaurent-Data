0d83e9bef2f20a7038bbd13100f919f98dcd83bc
Usama Shaikh <48800317+Usamashaikh744@users.noreply.github.com>
2019-07-13 13:12:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,506 @@
+let kolachiRestaurent
+let restaurent = {
+    "informationAboutRestaurent":{ 
+        owner: 4,
+        workers: 50,
+        chief: 10,
+        manager: 5,
+        tables: 100,
+        chairs: 300,    
+        room: 10,
+        camaras: 5,
+        computer: 5,
+        aircooler: 10,
+        fridge: 40,
+        pots: 1000,
+        fans: 20,
+        tissuesBox: 500,
+        spoons: 600,
+        eventsMoreThen: 20
+    },
+
+    "managments": {
+        members: [
+            name = "amir",
+            contactNumber = 0210546042,
+            serviceNo = "f11559",
+            cnic = 02136547714474, 
+            email = "amirasif123@gmail.com",
+            dateOfBirth = 5-10-1991, 
+            gender = "male",  
+            height = 6,
+            address = "defence",
+            joinCompany = "2011",
+            experience = "3years expereince",
+            hobbies = [
+                "cooking",
+                "booksReading",
+                "study"
+            ]
+
+            [
+                name = "asif",
+                contactNumber = 023015475,
+                serviceNo = "47895", 
+                cnic = 02136547714474,
+                email = "asif124@gmail.com",
+                dateOfBirth = 8-12-1998, 
+                gender = "male",  
+                height = 8,
+                address = "bahadurabad",
+                joinCompany = "2015",
+                experience = "1years expereince",
+                hobbies = [
+                    "games",
+                    "exercise",
+                    "books"
+                ]
+            ],
+
+                name = "ammar",
+                contactNumber = 02332561,
+                serviceNo = "4895", 
+                cnic = 021365477147144,
+                email = "ammar125@gmail.com",
+                dateOfBirth = 4-5-1989, 
+                gender = "male",  
+                height = 9,
+                address = "nazmabad",
+                joinCompany = "2010",
+                experience = "5years expereince",
+                hobbies = [
+                    "games",
+                    "exercise",
+                    "books"
+                ]
+        ],
+        
+    
+            name = "maryam",
+            contactNumber = 0124565,
+            serviceNo = "489895", 
+            cnic = 021254647147,
+            email = "maryam124@gmail.com",
+            dateOfBirth = 1-9-2000, 
+            gender = "female",  
+            height = 7,
+            address = "tariqRoad",
+            joinCompany = "2017",
+            experience = "1years expereince",
+            hobbies = [
+                "makeup",
+                "tv",
+                "study"
+            ]
+    },
+
+    "menuCard": {
+        menuCard: [
+            "chickenKrahi",
+            "muttonKarahi",
+            "biefkarahi",
+            "biryani",
+            "qourma",
+            "mukkonKarahi",
+            "broast",
+            "zingerBurger",
+            "kebab",
+            "seekhKebab",
+            "fish",
+            "shurwarma",
+            "fingerFish",
+            "vegetable",
+            "pizza",
+            "sandwich",
+            "legPiece",
+            "drinks",
+            "water",
+            "raita",
+        ]
+    },
+    
+    "home delivery": {
+
+        chickenKrahi: "rs.100",
+        muttonKarahi: "rs.200",
+        biefkarahi: "rs.300",
+        biryani: "rs.400",
+        qourma: "rs.400",
+        mukkonKarahi: "rs.1000",
+        broast: "rs.20",
+        zingerBurger: "rs.800",
+        kebab: "rs.350",
+        seekhKebab: "rs.250",
+        fish: "rs.800",
+        shurwarma: "rs.70",
+        fingerFish: "rs.400",
+        vegetable: "rs.50",
+        pizza: "rs.500",
+        sandwich: "rs.150",
+        legPiece: "rs.200",
+        drinks: "rs.50",
+        water: "rs.60",
+        raita: "rs.20",
+
+
+    },
+    "online delivery": {
+        "orderingFoodThrough": [
+            "mobileApp",
+            "ownWebsite",
+            "googleApp",
+            "googleMap",
+            "call:0424241212"
+        ]
+    },
+
+    "delivery information system": {
+        // Customer Details (From the Call center):
+        CustomersName: "usaid",
+        CustomersAddress: "azizanad",
+        PaymentMode: "still working",
+        PizzaMenuItems: "thirty items",
+        PizzaDeliveryPersonals: "rehan",
+        GeneralDetails: "read details",
+        PizzaTitlesPricesSizesEtc: "read details",
+        Process: "wants happy to customer",
+        Storage: "64",
+        CustomerDetails: "read details about customer",
+        Outletinformation: "more information",
+        Communication: "through calls and messanger",
+        Processorchip: "1.1GHz",
+        MBMemory: "256MB",
+        Harddisk: "20GB",
+        Keyboard: "ds",
+        MediumDeliverPizzas: "diffrent prices",
+        StandardDeliverySpeedWithinTheGridIs: "40 KM/h",
+    },
+
+    "discounts": {
+        eidOffer: "1buy 1free",
+        partyDay: "10% off",
+        birthdayPartyOffer: "20% off",
+        ramzanOffer: "every day 20% off",
+        christmasDay: "10% off",
+        independenceDay: "50% off",
+        motherDay: "1extra karahi 1 up to  free",
+        sunday: "5% off"
+    },
+
+
+    "events":{
+       "pizzaHutEvents":[
+            "eid party",
+            "birth day party",
+            "world cup",
+            "mother day",
+            "new year party",
+            "party for others.."
+       ]
+    },
+    "staff": {
+        staffAdmin: [
+            name = "afzal",
+            number = 9352520952,
+            information = "officer",
+            idNumber = "fk85230-2",
+            duration = "9am 4am",
+            address = "dehli baldia",
+            height = "23",
+            hobbies = "nthing",
+            worker = "pizza deliver",
+            experience = "14years"
+        ],
+
+        secondAdmin: [
+            name = "laiq",
+            number = 9352520952,
+            information = "dliver",
+            idNumber = "fk85230-2",
+            duration = "9am 4am",
+            address = "dehli baldia",
+            height = "23",
+            hobbies = "nthing",
+            worker = "pizza deliver",
+            experience = "14years"
+        ]
+    },
+
+    "ultimate Flavor": {
+        cheesePizza: "flavour",
+        malaiPizza: "flavour",
+        tandooriPizza: "flavour",
+        spicyPizza: "flavour",
+        fajeetaPizza: "flavour",
+        shawarmaPizza: "flavour",
+        tiikaPizza: "flavour",
+        broastPizza: "flavour",
+        chikenPizza: "flavour",
+        muttonPizza: "flavour"
+    },
+
+    "daily Net income": {
+        staurday: 2500,
+        sunday: 3400,
+        monday: 5400,
+        tuesday: 2250,
+        wednessday: 7000,
+        thursday: 30000,
+        friday: 44000,
+    },
+
+    "expense": {
+        petrol: 20000,
+        electricBill: 50000,
+        gassBill: 20000,
+        waterBill: 10000,
+        guardSalary: 30000,
+        workerSalary: 40000,
+        tex: 10000,
+        servicesExpense: 1000,
+        carServiceExxpencs: 20000,
+        driver: 30000
+    },
+
+    "branches": {
+        northNazimabad: "branche1",
+        pilaza: "branche2",
+        dehliColony: "branche3",
+        sadar: "branche4",
+        newkarachi: "branche5",
+        sharayFaisal: "branche6",
+        ayeshaManzil: "branche7",
+        doDarya: "branche8",
+        defence: "branche9",
+        PECSH: "branche10",
+        azizAbad: "branche11",
+        tariqRoad: "branche12",
+        husainAbad: "branche13",
+        burnsRoad: "branche14",
+        universityRoad: "branche15"
+    },
+
+    "workers": {
+        name: [
+            "rohit",
+            "kohli",
+            "pandya",
+            "rahul",
+            "boomra",
+            "salman",
+            "afzal",
+            "shookat",
+            "shakoor",
+            "ahsan",
+            "shani",
+            "arsalan",
+            "sufyan",
+            "awad",
+            "fawad",
+            "danish",
+            "muneeb"
+        ]
+    },
+    "cashier": {
+        name: [
+            name = "shoukat",
+            phone = 0324241344,
+            cnic =01541474747,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            dateOfBirth = "12 aug 2000",
+            gender = "male",
+            height = 6,
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        ],
+        secondPerson: [
+            name = "hanif",
+            phone = 0324241344,
+            cnic =01541474747,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            dateOfBirth = "12 aug 2000",
+            gender = "male",
+            height = 6,
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        ]
+    },
+    "kitchen Staff": {
+        name: [
+            name = "waqas",
+            phone = 0324241344,
+            cnic =01541474747,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            dateOfBirth = "12 aug 2000",
+            gender = "male",
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        ],
+        secondPerson: [
+            name = "azhar",
+            phone = 0324241344,
+            cnic =01541474747,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            dateOfBirth = "12 aug 2000",
+            gender = "male",
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        ],
+
+        thirdPerson: [
+            name = "hanif",
+            phone = 0324241344,
+            cnic =01541474747,
+            idNumber = "fsj12363",
+            address = "dehli colony 1732/43",
+            dateOfBirth = "12 aug 2000",
+            gender = "male",
+            email = "maazmaaaz@gmail.com",
+            experience = "4years expereince",
+            joiningCompany = "2014 5 07",
+        ]
+        
+    },
+
+    "bonus": {
+        weeklyBonus: [
+            5500000,
+            3500000,
+            9500000,
+            8500000,
+            7900000,
+        ],
+        monthlyBonus: [
+            12500000,
+            123500000,
+            129500000,
+            822500000,
+            127900000,
+        ]
+    },
+
+    "employment": {},
+    "salery": {
+        "kitchen Staff salary": [
+            25000,
+            28000,
+            29000,
+        ],
+
+        "worker salary": [
+            25000,
+            28000,
+            29000,
+        ],
+
+        "worker salary": [
+            25000,
+            28000,
+            29000,
+        ]
+    },
+    "Planning": {
+        planning: [
+            "no complain with staff",
+            "Surver to best",
+            "respect others",
+            "best communicate",
+            "no cheating with customer",
+            "no comp[lain about food"
+        ]
+    },
+
+    "Security": {
+        cameras: "Security systems",
+        guards: "Securitysystems",
+        workers: "Security systems",
+        ourTeam: "Security systems",
+        cyber: "Security systems",
+        securityForces: "security",
+        ethical: "Security ",
+        digital: "Security ",
+        national: "Security ",
+        public: "security",
+        physicalSecurity: "security",
+    },
+
+    "clening": {
+        clening: [
+            "tableWash",
+            "potsWashes",
+            "roadCleaninng",
+            "tylesWash",
+        ]
+    },
+
+    "General process till enjoying a Pizza at the door step": {
+        PizzaHutHotline: 03257474761,
+        ClientsName: "Mr Hameed",
+        ClientsPhoneNumber: 03126645,
+        ClientsName: "Mr Usman",
+        ClientsContactNumber: 0234556665,
+        Address: "1823/466 NewZealand",
+        // When they call back if the customer is a fresh person following details are added to their database,
+    },
+
+    "chief": {
+        // about cheif
+        "amirChief": [
+            WorkExperience = "10years",
+            salary = 100000,
+            duration = "10am 3pm",
+            IDcard = "23dakfalf",
+            profession = "cheif",
+            age = 55,
+            number = 0342525534,
+            hobbies = "cheif",
+            freeTime = "codding",
+            wnats = "world master cheif",
+        ],
+
+        "shoukatCheif": [
+            WorkExperience = "10years",
+            salary = 100000,
+            duration = "10am 3pm",
+            IDcard = "23dakfalf",
+            profession = "cheif",
+            age = 55,
+            number = 0342525534,
+            hobbies = "cheif",
+            freeTime = "codding",
+            wnats = "world master cheif",
+        ],
+
+        "latifCheif": [
+            WorkExperience = "10years",
+            salary = 100000,
+            duration = "10am 3pm",
+            IDcard = "23dakfalf",
+            profession = "cheif",
+            age = 55,
+            number = 0342525534,
+            hobbies = "cheif",
+            freeTime = "codding",
+            wnats = "world master cheif",
+        ]
+    },
+
+    "call center": {
+        callUs: [
+            0352552563,
+            0343564675,
+            0321342141,
+            0325433421,
+            0321345211,
+            0321345211,
+        ]
+    },
+};
